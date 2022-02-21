728694364ef5cc2f5d74c9982fb2840ff76ee904
Thirumoorthi S <90926526+sthirumoorthi@users.noreply.github.com>
2022-02-21 23:01:52 +0530

    Add files via upload

---
+++ Python/MongoDB Bulk Data Load/MongoDataLoad/MongoDB_config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+CLIENT_URL = "mongodb+srv://test:test@cluster0.3tlei.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DB_NAME = "Carbon_Nanotubes_DB"
+COLL_NAME = "Carbon_Nanotubes_COLL"
+FILE_PATH = "carbon_nanotubes.csv"
+
